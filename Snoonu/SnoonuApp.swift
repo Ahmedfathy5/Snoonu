454359769fd3e64007c4319eb64b0f7014cd202b
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-09-08 23:25:27 +0300

    Initial Commit

---
+++ Snoonu/SnoonuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoonuApp.swift
+//  Snoonu
+//
+//  Created by Ahmed Fathi on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnoonuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
